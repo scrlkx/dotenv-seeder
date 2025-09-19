b74fd40050acd82fa5e2405af7b33dc4e300643d
Daniel Freitas <git@scrlkx.com>
2025-09-19 12:00:33 -0300

    feat: scape values with common slash

--- src/index.ts
+++ src/index.ts
@@ -2,50 +2,58 @@ import * as fs from "fs";
 import * as dotenv from "dotenv";
 import * as core from "@actions/core";
 
-const target = core.getInput("target", {
-  required: false,
-  trimWhitespace: true,
-}) || ".env";
+const target =
+    core.getInput("target", {
+        required: false,
+        trimWhitespace: true,
+    }) || ".env";
 
-const example = core.getInput("example", {
-  required: false,
-  trimWhitespace: true,
-}) || ".env.example";
+const example =
+    core.getInput("example", {
+        required: false,
+        trimWhitespace: true,
+    }) || ".env.example";
 
 const variables = core.getInput("variables", {
-  required: true,
-  trimWhitespace: false,
+    required: true,
+    trimWhitespace: false,
 });
 
 try {
-  if (!fs.existsSync(target)) {
-    console.log(`Copying ${example} to ${target}`);
-    fs.copyFileSync(example, target);
-  }
+    if (!fs.existsSync(target)) {
+        console.log(`Copying ${example} to ${target}`);
+        fs.copyFileSync(example, target);
+    }
 
-  const input: Record<string, string> = JSON.parse(variables);
+    const input: Record<string, string> = JSON.parse(variables);
 
-  // Load the existing environment file (if it exists)
-  const existent = fs.existsSync(target)
-    ? dotenv.parse(fs.readFileSync(target, "utf-8"))
-    : {};
+    // Load the existing environment file (if it exists)
+    const existent = fs.existsSync(target)
+        ? dotenv.parse(fs.readFileSync(target, "utf-8"))
+        : {};
 
-  // Merge existing variables with new ones
-  const result = { ...existent, ...input };
+    // Merge existing variables with new ones
+    const result = {
+        ...existent,
+        ...input,
+    };
 
-  const handledResult = Object.entries(result)
-    .map(([key, value]) => {
-      // Wrap value in quotes if it contains spaces
-      const escapedValue = value.includes(" ") ? `"${value}"` : value;
+    const handledResult = Object.entries(result)
+        .map(([key, value]) => {
+            // Wrap value in quotes if it contains special characters
+            const escapedValue =
+                value.includes(" ") || value.includes("/")
+                    ? `"${value}"`
+                    : value;
 
-      return `${key}=${escapedValue}`;
-    })
-    .join("\n");
+            return `${key}=${escapedValue}`;
+        })
+        .join("\n");
 
-  fs.writeFileSync(target, handledResult, "utf-8");
+    fs.writeFileSync(target, handledResult, "utf-8");
 
-  console.log(`Updated ${target} successfully!`);
+    console.log(`Updated ${target} successfully!`);
 } catch (error) {
-  console.error(error);
-  core.setFailed("Error processing the environment file");
+    console.error(error);
+    core.setFailed("Error processing the environment file");
 }
